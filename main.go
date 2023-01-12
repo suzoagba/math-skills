b9d3c45df53d9b935890fe91d26ae8128192f0d5
suzoagba <samuzoagba@gmail.com>
2023-01-12 18:47:23 +0200

    math-skills

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"math"
@@ -7,53 +8,53 @@ import (
 	"strconv"
 	"strings"
 )
-//this is a test
-func main() {	
-	data, err := os.ReadFile("data.txt")  			
+
+func main() {
+	data, err := os.ReadFile("data.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	line := string(data)
+	str := strings.Split(line, "\n")
+
+	var f []float64
+	for _, data1 := range str {
+		num, err := strconv.ParseFloat(data1, 64)
 		if err != nil {
 			fmt.Println(err)
-		}	
-	
-	line := string(data) 								
-	str := strings.Split(line, "\r\n")
-	
-	var f []float64 
-		for _, data1 := range str  {						
-			num, err := strconv.ParseFloat(data1, 64) 
-			if err != nil {
-				fmt.Println(err)
-				}			
-			f = append(f, num)							
-		}	
-		fmt.Println(count(f))
-	}	
+		}
+		f = append(f, num)
+	}
+	fmt.Println(count(f))
+}
 func count(data []float64) float64 {
-    dataCopy := make([]float64, len(data))
-    copy(dataCopy, data)
-    sort.Float64s(dataCopy)
-	var sum float64 = 0							
+	dataCopy := make([]float64, len(data))
+	copy(dataCopy, data)
+	sort.Float64s(dataCopy)
+	var sum float64 = 0
 	for i := 0; i < len(dataCopy); i++ {
 		sum += dataCopy[i]
 	}
-	var average float64 = sum / float64(len(dataCopy)) 		
+	var average float64 = sum / float64(len(dataCopy))
 	fmt.Println("Average:\n", math.Round(average))
-    var median float64                                
-    l := len(dataCopy)
-    if l == 0 {
-        return 0
-    } else if l%2 == 0 {										
-        median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
-    } else {
-        median = dataCopy[l/2]
-    }
+	var median float64
+	l := len(dataCopy)
+	if l == 0 {
+		return 0
+	} else if l%2 == 0 {
+		median = (dataCopy[l/2-1] + dataCopy[l/2]) / 2
+	} else {
+		median = dataCopy[l/2]
+	}
 	fmt.Println("Median:\n", math.Round(median))
 	var sd float64
-    for j := 0; j < 10; j++ {	
-        sd += math.Pow(dataCopy[j] - average, 2)	
-		fmt.Println(sd)		
-    }
+	for j := 0; j < l; j++ {
+		sd += math.Pow(dataCopy[j]-average, 2)
+		fmt.Println(sd)
+	}
 	fmt.Println("Variance:\n", math.Round(sd))
-    sd = math.Sqrt(sd/10)						
+	sd = math.Sqrt(sd / 10)
 	fmt.Println("Standard Deviation:")
 	return math.Round(sd)
-} 
+}
